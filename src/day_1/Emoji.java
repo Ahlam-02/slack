b52e4e3abce1b06664bdf8ef7601d6eb948a27b3
Ahlam-02 <ahlaamomar1982@gmail.com>
2020-05-13 18:08:16 +0200

    add Emoji.java

---
+++ src/day_1/Emoji.java
@@ -0,0 +1,21 @@
+package day_1;
+
+public class Emoji {
+
+    public static void main(String[] args) {
+
+        System.out.println("User should be able to add any Emoji  :)");
+
+    }
+
+
+
+
+
+
+    //git branch commend--->* master so you are in the activ branch
+    //git branch branch name---> creating a branch
+    //git checkout branchname -->it will switch to the branch
+}
+
+
